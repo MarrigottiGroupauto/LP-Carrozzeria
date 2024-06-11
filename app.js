ed554689da0848cbb93d06e879b2adb95308512a
Mattia Arrigotti <m.arrigotti@groupauto.it>
2024-06-11 15:57:39 +0000

    not working well, but requestes are fine

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
-import express from "express";
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
 
-import path from "path";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+const key = "b1af733907eb45a3a9bf606d7834a5dd";
+const endpoint = "https://westeurope.api.cognitive.microsoft.com/";
+const modelId = "LP-CARR-AUT_Nerural"
 
-import {ciao} from "./azure-interface.mjs"
+app.use(express.json()); // Middleware to parse JSON bodies
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const PORT = 3000;
+// Route to handle document recognition requests
+app.post('/recognize-document', async (req, res) => {
+    const { documentUrl } = req.body;
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/pages/index/index.html"));
+    if (!documentUrl) {
+        return res.status(400).send('Document URL is required');
+    }
+
+    // POST request, useful for fetching the API key for the actual request
+    // {endpoint}/documentintelligence/documentModels/{modelId}:analyze?api-version=2024-02-29-preview
+    // HEADER :'Ocp-Apim-Subscription-Key': key
+    try {
+        const keyFetchResponce = await axios.post(`${endpoint}/documentintelligence/documentModels/${modelId}:analyze?api-version=2024-02-29-preview`,
+            {
+                urlSource: documentUrl
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Ocp-Apim-Subscription-Key': key
+                }
+            });
+
+        apimId = keyFetchResponce.headers["apim-request-id"];
+
+        console.log(apimId);
+
+        // Actual GET request
+        // {endpoint}/documentintelligence/documentModels/{modelId}/analyzeResults/{apimId}?api-version=2024-02-29-preview
+        // HEADER :'Ocp-Apim-Subscription-Key': key
+
+        const analysisResponce = await axios.get(`${endpoint}/documentintelligence/documentModels/${modelId}/analyzeResults/${apimId}?api-version=2024-02-29-preview`,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Ocp-Apim-Subscription-Key': key
+                }
+            });
+
+        console.log(analysisResponce);
+        res.send("analysisResponce")
+
+    } catch (error) {
+        console.error('Error recognizing document:', error);
+        res.status(500).send(`${error}`);
+    }
 });
 
-app.listen(PORT, () => console.log(`Server partito, porta ${PORT}`));
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
